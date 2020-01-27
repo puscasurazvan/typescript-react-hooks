465e889b9e3d16dbb6975f04c81fe2f480f48c02
Razvan Puscasu <puscasurazvan93@gmail.com>
2020-01-27 21:10:48 +0000

    createContext ato create GlobalState Provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,24 @@ import './App.css'
 import Header from './components/Header'
 import Button from './components/Button'
 import Input from './components/Input'
+import { GlobalProvider } from './components/GlobalState'
 import ReducerButtons from './components/ReducerButtons'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Header title='Hello' />
-      <Button onClick={(event) => {
-        event.preventDefault()
-        console.log(event)
-      }}>
-        Hello world!
-      </Button>
-      <ReducerButtons />
-      <Input />
-    </div>
+    <GlobalProvider>
+      <div className="App">
+        <Header title='Hello' />
+        <Button onClick={(event) => {
+          event.preventDefault()
+          console.log(event)
+        }}>
+          Hello world!
+        </Button>
+        <ReducerButtons />
+        <Input />
+      </div>
+    </GlobalProvider>
   )
 }
 
--- src/components/ReducerButtons.tsx
+++ src/components/ReducerButtons.tsx
@@ -1,41 +1,24 @@
-import React, { useReducer, useRef } from 'react'
+import React, { useRef, useContext } from 'react'
 import { useClickOutside } from './useClickOutside'
-
-const initialState = {
-  rValue: true,
-}
-
-type Action = { type: 'one' } | { type: 'two' }
-
-type State = {
-  rValue: boolean,
-}
-
-function reducer(state: State, action: Action) {
-  switch (action.type) {
-    case 'one':
-      return { rValue: true }
-    case 'two':
-      return { rValue: false }
-    default:
-      return state
-  }
-}
+import { GlobalContext } from './GlobalState'
 
 const ReducerButtons = () => {
-  const [state, dispatch] = useReducer(reducer, initialState)
   const ref = useRef<HTMLDivElement>(null!)
+
+  const context = useContext(GlobalContext)
+  const { rValue, turnOn, turnOff } = context
+
   useClickOutside(ref, ()=>{
     console.log('clicked outside')
   })
 
   return (
     <div ref={ref} >
-      {state && state.rValue && 
+      {rValue &&
         <h1>Visible</h1>
       }
-      <button onClick={() => dispatch({type: 'one'})}>Action One</button>
-      <button onClick={() => dispatch({type: 'two'})}>Action Two</button>
+      <button onClick={turnOn}>Action One</button>
+      <button onClick={turnOff}>Action Two</button>
     </div> 
   )
 }
